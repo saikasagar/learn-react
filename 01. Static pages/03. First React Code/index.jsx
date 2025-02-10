a4b5c4288a578c0b2d2c7e110f75d2eddfa03b84
Sagar dahal <60807150+saikasagar@users.noreply.github.com>
2025-02-10 23:41:41 +0545

    Update index.jsx

--- 01. Static pages/03. First React Code/index.jsx
+++ 01. Static pages/03. First React Code/index.jsx
@@ -1,3 +1,12 @@
+import {createRoot} from "react-dom/client"
+createRoot(document.getElementById("root")).render(
+  <ul>
+    <li>First</li>
+    <li>Second</li>
+    <li>Third</li>
+  </ul>
+)
+
 // Challenge: Re-write the first lines of React code we just had.
 // You can render anything you want to the screen, doesn't have
 // to be an h1 element.
